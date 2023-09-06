675796da0ad1c31d9d514ce7034ec349a9470356
Dhruv Tomar <134826881+tomardhruv32@users.noreply.github.com>
2023-09-06 20:08:10 +0530

    Update Sidebar.jsx

--- frontend/src/components/Sidebar/Sidebar.jsx
+++ frontend/src/components/Sidebar/Sidebar.jsx
@@ -25,7 +25,7 @@ const Sidebar = () => {
       <div className='top'>
         <div className='top'>
           <Link to='/' style={{ textDecoration: 'none', color: '#222222' }}>
-            <span className='logo'>EasyPay</span>
+            <span className='logo'>Paymento</span>
           </Link>
         </div>
       </div>
